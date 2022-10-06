1534a0e5794d7c172d55bbaddfa90e81894a011f
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-10-05 18:09:19 -0700

    created Home page, and added tests for navigation.js and home.js. All test passed.

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
-const Home = () => {
+
+const Home = ({logged_in,
+    current_user
+    }) => {
     return (
     <>
-        <h1>This is the Home</h1>
+        <h1>Welcome to simPill</h1>
+        {!logged_in && (
+     <>
+        <h2>Giving you the power to control your health</h2>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nullam non nisi est sit amet facilisis magna etiam. Lacinia quis vel eros donec ac odio tempor. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus.
+        </p>
+     </>
+        )}
+        {logged_in && (
+     <>
+        <h2>Welcome back {current_user.first_name}</h2>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nullam non nisi est sit amet facilisis magna etiam. Lacinia quis vel eros donec ac odio tempor. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus.
+        </p>
+     </>
+        )}
     </>
     )
 }
