a8aafecec9ee864a67c849fc69ca46fcfbdb9039
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-10-07 11:02:30 -0700

    validations models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :medications
+
+  belongs_to :user
+  validates :first_name, :last_name, :email, :password, presence: true
+
+
 end
