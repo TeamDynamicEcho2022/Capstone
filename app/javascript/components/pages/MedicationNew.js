740e7e38b4b192133e363a437e684a4c45563e7a
JPD99 <jeremedoan@gmail.com>
2022-10-08 15:03:56 -0700

    useState of newMedication has been updated to mirror schema, additional indentation has been made to improve readability

--- app/javascript/components/pages/MedicationNew.js
+++ app/javascript/components/pages/MedicationNew.js
@@ -9,7 +9,8 @@ const MedicationNew = ({createMedication}) => {
       description: "",
       strength: "",
       frequency: "",
-      image: ""
+      image: "",
+      is_taken: "false"
     })
     const handleChange = (e) => {
       setNewMedication({...newMedication, [e.target.name]: e.target.value})
@@ -23,38 +24,77 @@ const MedicationNew = ({createMedication}) => {
         <h1>Add a New Medication Below</h1>
         <Form>
           <FormGroup>
-            <Label for="drug_name">Drug Name: </Label>
-            <Input type="text" name="drug_name" placeholder="Enter Drug Name" onChange={handleChange} value={newMedication.drug_name}/>
+            <Label for="drug_name"> Drug Name: </Label>
+            <Input 
+            type="text" 
+            name="drug_name" 
+            placeholder="Enter Drug Name" 
+            onChange={handleChange} 
+            value={newMedication.drug_name}
+            />
           </FormGroup>
           <FormGroup>
-            <Label for="drug_cat">Drug Category: </Label>
-            <Input type="text" name="drug_cat" placeholder="Enter Drug Category" onChange={handleChange} value={newMedication.drug_cat}/>
+            <Label for="drug_cat"> Drug Category: </Label>
+            <Input t
+            ype="text" 
+            name="drug_cat" 
+            placeholder="Enter Drug Category" 
+            onChange={handleChange} 
+            value={newMedication.drug_cat}
+            />
           </FormGroup>
           <FormGroup>
-            <Label for="description">Description: </Label>
-            <Input type="text" name="description" placeholder="Enter Description" onChange={handleChange} value={newMedication.description}/>
+            <Label for="description"> Description: </Label>
+            <Input 
+            type="text" 
+            name="description" 
+            placeholder="Enter Description" 
+            onChange={handleChange} 
+            value={newMedication.description}
+            />
           </FormGroup>
           <FormGroup>
-            <Label for="strength">Strength: </Label>
-            <Input type="text" name="strength" placeholder="Enter Strength" onChange={handleChange} value={newMedication.strength}/>
+            <Label for="strength"> Strength: </Label>
+            <Input 
+            type="text" 
+            ame="strength" 
+            placeholder="Enter Strength" 
+            onChange={handleChange} 
+            value={newMedication.strength}
+            />
           </FormGroup>
           <FormGroup>
-            <Label for="frequency">Frequency: </Label>
-            <Input type="text" name="frequency" placeholder="Enter Frequency" onChange={handleChange} value={newMedication.frequency}/>
+            <Label for="frequency"> Frequency: </Label>
+            <Input 
+            type="text" 
+            name="frequency" 
+            placeholder="Enter Frequency" 
+            onChange={handleChange} 
+            value={newMedication.frequency}
+            />
           </FormGroup>
           <FormGroup>
-            <Label for="image">Upload Image url: </Label>
-            <Input type="text" name="image" placeholder="Enter Upload Image URL" onChange={handleChange} value={newMedication.url}/>
+            <Label for="image"> Upload Image url: </Label>
+            <Input 
+            type="text" 
+            name="image" 
+            placeholder="Enter Upload Image URL" 
+            onChange={handleChange} 
+            value={newMedication.url}
+            />
           </FormGroup>
         </Form>
         <NavLink to="/">
-          <Button color="primary" onClick={handleSubmit} name="submit">
+          <Button 
+          color="primary" 
+          onClick={handleSubmit} 
+          name="submit">
             Submit New Medication Form
           </Button>
         </NavLink>
-        
       </>
     )
   }
+  
   
   export default MedicationNew
