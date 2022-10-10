02cdc71d4288260bfa9c919948ade706bb26429c
dnlclmnt <dnlclmnt@gmail.com>
2022-10-10 00:49:33 -0400

    added delete spec request tests and 422 errors display for unprocessable requests. also added default value of false for is_taken column. it will not pass validations so giving a default will ensure that it is always set

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_141023) do
     t.string "strength"
     t.string "frequency"
     t.string "image"
-    t.boolean "is_taken"
+    t.boolean "is_taken", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_medications_on_user_id"
--- spec/requests/medications_spec.rb
+++ spec/requests/medications_spec.rb
@@ -87,4 +87,83 @@ RSpec.describe "Medications", type: :request do
     expect(drugHash[:drug_name]).to eq("editedtestdrug")
     end
   end
+  describe "DELETE /users/#{user.id}/medications/:id" do
+  it 'deletes an existing medication from within the database' do
+    user.medications.create(
+        drug_name: 'testdrug',
+        drug_cat: 'uncategorizable',
+        description: 'whatever',
+        strength: '10000mcg',
+        frequency: 'all the time',
+        image: 'image.com',
+        is_taken: false,
+        user_id: user.id)
+
+    deleteMedication = Medication.first
+
+    delete "/users/#{user.id}/medications/#{deleteMedication.id}"
+    expect(response.status).to eq(204)
+    expect(Medication.count).to eq 0
+    end
+  end
+
+  describe "POST /create 422 error" do
+    it 'has a response of 422 if creating a new medication is not valid' do
+    med_params = {
+      medication: {
+        drug_cat: 'uncategorizable',
+        description: 'whatever',
+        strength: '10000mcg',
+        frequency: 'all the time',
+        image: 'image.com',
+        is_taken: false,
+        user_id: user.id
+      }
+    }
+
+    post "/users/#{user.id}/medications", params: med_params
+
+    medication = Medication.last
+
+    expect(response).to have_http_status(422)
+    expect(Medication.count).to eq 0
+    end
+  end
+
+  describe "PATCH /users/:user_id/medications/:id 422 error" do
+    it 'has an HTTP response of 422 if medication update is not valid' do
+      med_params = {
+        medication: {
+          drug_name: 'testdrug',
+          drug_cat: 'uncategorizable',
+          description: 'whatever',
+          strength: '10000mcg',
+          frequency: 'all the time',
+          image: 'image.com',
+          is_taken: false,
+          user_id: user.id
+        }
+    }
+
+    post "/users/#{user.id}/medications", params: med_params
+
+    edit_params = {
+      medication: {
+        drug_name: 'editedtestdrug',
+        drug_cat: 'editeduncategorizable',
+        description: 'whatever',
+        strength: '10000mcg',
+        frequency: 'all the time',
+        image: '',
+        is_taken: false,
+        user_id: user.id
+      }
+    }
+
+    medication = Medication.first
+
+    patch "/users/:user_id/medications/#{medication.id}", params: edit_params
+    expect(response).to have_http_status(422)
+    end
+  end
 end
